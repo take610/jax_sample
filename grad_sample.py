ed8e0f8aa4e22ce31c4f574c0caeb8bdb97421c0
takeuchi <>
2020-11-30 12:07:53 +0900

    update mnist samples

--- grad_sample.py
+++ grad_sample.py
@@ -1,8 +1,7 @@
-import jax
-import jax.numpy as jnp
-from jax import grad, jit, vmap, random
-
-# grad sample
+"""
+sample code for testing jax auto-grad system
+"""
+from jax import grad
 
 
 def f(x1, x2):
@@ -11,10 +10,15 @@ def f(x1, x2):
     return x1 ** 2 + x1 * x2 + x2 ** 2
 
 
-x1 = 1.0
-x2 = 1.0
-x1_grad = grad(f, argnums=0)(x1, x2)
-print(f"x1 grad: {x1_grad}")
-x2_grad = grad(f, argnums=1)(x1, x2)
-print(f"x2 grad: {x2_grad}")
+def main():
+    x1 = 1.0
+    x2 = 2.0
+    x1_grad = grad(f, argnums=0)(x1, x2)
+    print(f"x1 grad: {x1_grad}")  # -> 4.0
+    x2_grad = grad(f, argnums=1)(x1, x2)
+    print(f"x2 grad: {x2_grad}")  # -> 5.0
+
+
+if __name__ == "__main__":
+    main()
 
